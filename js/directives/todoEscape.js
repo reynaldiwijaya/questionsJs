d983f2e5be94511dfc4d8866dd02337c25f8dad1
Reynaldi Wijaya <reynaldioctavianus@gmail.com>
2015-09-20 22:15:34 +0800

    Change on double duplicated directive
    At todoEscape.js, I found that the directive for todoEscape is same
    with todoBlur. I am not sure if using identical name for two directives
    is allowed

--- js/directives/todoEscape.js
+++ js/directives/todoEscape.js
@@ -5,12 +5,13 @@
 * Directive that executes an expression when the element it is applied to gets
 * an `escape` keydown event.
 */
-todomvc.directive('todoBlur', function () {
+todomvc.directive('todoEscape', function () {
 	var ESCAPE_KEY = 27;
 	return function (scope, elem, attrs) {
-		elem.bind('keydown', function (event) {
+        elem.bind('keydown', function (event) {
 			if (event.keyCode === ESCAPE_KEY) {
 				scope.$apply(attrs.todoEscape);
+                  
 			}
 		});
 
